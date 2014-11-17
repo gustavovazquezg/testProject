8f143e95ba8523a96c3932744bd67daa7605f8c3
Gustavo Vazquez <gustavovazquezg@hotmail.com>
2014-11-17 09:25:31 -0800

    Added Hello World Application
    Added all the files of an ASP.net application, very basic

---
+++ HelloWorld/KilnTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace KilnTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("And what's your sur name?");
+            string surName = Console.ReadLine();
+            Console.WriteLine("And how old?");
+            string howOld = Console.ReadLine();
+            Console.Write("Your full name is {0} {1} and age {2}... see you later!", name, surName, howOld);
+            Console.ReadLine();
+        }
+    }
+}
